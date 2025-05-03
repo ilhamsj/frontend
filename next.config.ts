f2a1ebdf988fdc9bd95fdbd32b98163d8fdb4642
ilhamsj <ilhamsaputrajati@gmail.com>
2025-05-03 19:12:45 +0700

    Add allowedDevOrigins to Next.js config, update dependencies, and refactor styles

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // allowedDevOrigins
+  allowedDevOrigins: ["local.wordpress.test"],
 };
 
 export default nextConfig;
