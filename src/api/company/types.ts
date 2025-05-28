04b189e6fc1a9a6b9c1d4bf09f0bf58de39edb86
ilhamsj <ilhamsaputrajati@gmail.com>
2025-05-29 06:57:50 +0700

    Refactor SearchCompaniesRequest and FacetFilter types for improved clarity and structure in company API types.

--- src/api/company/types.ts
+++ src/api/company/types.ts
@@ -5,15 +5,16 @@ export type GetCompaniesFilterRequest = Pagination & {
   location?: string;
   category?: string;
 };
+
+export type SearchCompaniesRequest = {
+  page?: number;
+  hitsPerPage?: number;
+  query?: string;
+  sort?: SortOption;
+} & FacetFilter;
+
 export type GetCompaniesFilterResponse = ResourceResults<Company[]>;
 export type GetCompaniesHitsResponse = Hit<Company[]>;
-
-export type FacetFilter = {
-  batch?: string[];
-  industry?: string[];
-  regions?: string[];
-  stage?: string[];
-};
 
 export type SortOption =
   | "launched_at:desc"
@@ -22,9 +23,9 @@ export type SortOption =
   | "name:desc"
   | "";
 
-export type SearchCompaniesRequest = {
-  page?: number;
-  hitsPerPage?: number;
-  query?: string;
-  sort?: SortOption;
-} & FacetFilter;
+export type FacetFilter = {
+  batch?: string[];
+  industry?: string[];
+  regions?: string[];
+  stage?: string[];
+};
